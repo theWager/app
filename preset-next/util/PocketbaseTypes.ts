2506e2b74dcda01e84096af90732d24ea549c09c
Bruno Grbavac <bruno.grbavac@outlook.com>
2024-09-08 22:35:58 +0200

    fix: pocketbase mapping refactor

---
+++ preset-next/util/PocketbaseTypes.ts
@@ -0,0 +1,27 @@
+export type PocketBaseBet = {
+  id: string
+  collectionId: string
+  collectionName: string
+  created: string
+  updated: string
+  address_creator: string
+  address_opponent: string
+  address_judge: string
+  category: string
+  expire_date: string
+  end_date: string
+  bet_hash: string
+  description: string
+  title: string
+  odd_created: number
+  odd_opponent: number
+  amount: number
+  accepted_opponent: boolean
+  accepted_judge: boolean
+  judge_fee: number
+}
+
+export type PocketBaseUser = {
+    name: string,
+    address: string,
+}
