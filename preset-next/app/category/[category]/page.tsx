621e090ab44fb0d327897fb099ad66f366cba986
Ivan Lukšić <ivanluksic1@gmail.com>
2024-10-20 18:08:48 +0200

    Category fix

--- preset-next/app/category/[category]/page.tsx
+++ preset-next/app/category/[category]/page.tsx
@@ -4,9 +4,11 @@ import AllBetsHeader from '@/sections/AllBets'
 import { BetPages } from '@/util/Enums'
 import { MOCK_BETS } from '@/util/Mocks'
 import { useParams } from 'next/navigation'
+import {useState} from 'react'
 
 export default function MyBets() {
   const { category } = useParams()
+  const [update, setUpdate] = useState(0)
 
   return (
     <DashboardLayout>
@@ -14,6 +16,7 @@ export default function MyBets() {
         <AllBetsHeader
           isLoggedIn
           title={`${category as string} Bets`}
+          update = {setUpdate}
           bets={MOCK_BETS}
           page={BetPages.MY_BETS}
         />
